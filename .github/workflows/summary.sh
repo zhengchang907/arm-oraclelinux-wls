8f9e0fdfa3ce3cac3aa3204647e5acb7145dd6b0
zhengchang <zhengchang@microsoft.com>
2021-02-22 14:21:08 +0800

    clean up code

--- .github/workflows/summary.sh
+++ .github/workflows/summary.sh
@@ -1,27 +1,25 @@
-
 #!/bin/bash
 
-latest_workflow=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/zhengchang907/arm-oraclelinux-wls/actions/runs?per_page=1&page=1)
-echo $latest_workflow
+run_id="$1"
+webhook="$2"
 
-latest_workflow_id=$(echo $latest_workflow | jq '.workflow_runs[0].id')
-echo $latest_workflow_id
-
-workflow_jobs=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/zhengchang907/arm-oraclelinux-wls/actions/runs/$latest_workflow_id/jobs)
-echo $workflow_jobs
-
+workflow_jobs=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/zhengchang907/arm-oraclelinux-wls/actions/runs/${run_id}/jobs)
 critical_job_num=$(echo $workflow_jobs | jq '.jobs | map(select(.name|test("^deploy."))) | length')
-echo $critical_job_num
-
+echo "$critical_job_num"
 succeed_critical_job_num=$(echo $workflow_jobs | jq '.jobs | map(select(.conclusion=="success") | select(.name|test("^deploy."))) | length')
-echo $succeed_critical_job_num
-
-echo $(( $critical_job_num - $succeed_critical_job_num ))
-if (( $(( $critical_job_num - $succeed_critical_job_num )) >= 2 ))
-then 
-    echo "failed, fire Teams event"
-else
-    echo "succeed"
+echo "$succeed_critical_job_num"
+failed_job_num="$(($critical_job_num-$succeed_critical_job_num))"
+echo $failed_job_num
+if (($failed_job_num >= 2));then
+    echo "too many jobs failed, send notification to Teams"
+    curl ${webhook} \
+    -H 'Content-Type: application/json' \
+    --data-binary @- << EOF
+    {
+    "@context":"http://schema.org/extensions",
+    "@type":"MessageCard",
+    "text":"$failed_job_num jobs failed in arm-oraclelinux-wls workflow, please take a look at: https://github.com/zhengchang907/arm-oraclelinux-wls/actions/runs/${run_id}"
+    }
+EOF
 fi
-
-exit 1
+exit 0
